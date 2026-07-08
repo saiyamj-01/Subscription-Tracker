d39d249cf2dc5de1dd2aad6e212c374050ac314b
saiyamj_01 <saiyam.ja01@gmail.com>
2026-07-08 08:01:33 +0530

    Subscription Tracker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+//vite.dev/config/
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env variables regardless of the 'VITE_' prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/Subscription-Tracker",
+  };
+});
